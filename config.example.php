7ac0ffd7443d2a3ae88ac85dc09138ae2385b2f1
Ed <edjash@gmail.com>
2017-06-10 14:06:25 +0100

    Added Example config

---
+++ config.example.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * An array of configration values, edit these values and rename the file to config.php
+ * @var array
+ */
+$config = array(
+	'debug'	   => true,
+	'base_url' => '/voting/',
+	'db_host'  => 'localhost',
+	'db_name'  => 'ed_voting_app',
+	'db_user'  => 'ed',
+	'db_pass'  => 'edvote',
+);
